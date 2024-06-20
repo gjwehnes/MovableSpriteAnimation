facdb060b261ba610203dbd153170387a6c43ff3
gjwehnes <gjwehnes@E829WL170006.org.cbe.ab.ca>
2024-06-19 18:31:28 -0600

    transition function for universe implemented

--- animations/MovableSpriteAnimation.java
+++ animations/MovableSpriteAnimation.java
@@ -1,18 +1,46 @@
 
 public class MovableSpriteAnimation implements Animation {
 
-	private int universeCount = 0;
+	private Universe current = new MovableSpriteUniverse();
+	private boolean universeSwitched = false;
+	private boolean animationComplete = false;
 	
-	public Universe getNextUniverse() {
+	public Universe switchUniverse(Object event) {
 
-		universeCount++;
+		universeSwitched = true;
+		//there is only the initial universe; when a switch is called for, set current to null;
+		animationComplete = true;
 		
-		if (universeCount == 1) {
-			return new MovableSpriteUniverse();
-		}
-		else {
-			return null;
-		}
+		return current;
+
+	}
+
+	public Universe getCurrentUniverse() {
+		return current;
+	}
+	
+	@Override
+	public boolean getUniverseSwitched() {
+		return universeSwitched;
+	}
+
+	@Override
+	public void acknowledgeUniverseSwitched() {
+		this.universeSwitched = false;		
+	}
+
+	@Override
+	public boolean isComplete() {
+		return animationComplete || current.isComplete();
+	}
+
+	@Override
+	public void setComplete(boolean complete) {
+		this.animationComplete = true;		
+	}
+
+	@Override
+	public void update(KeyboardInput keyboard, long actual_delta_time) {
 	}
 	
 }
