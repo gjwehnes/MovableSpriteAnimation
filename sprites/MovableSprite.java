e74326b17392f38f0119aeb46e1c1ab3b15b548a
gjwehnes <gjwehnes@E829WL240006.org.cbe.ab.ca>
2025-01-03 10:11:42 -0700

    added to interface for future implementation of collective animation

--- sprites/MovableSprite.java
+++ sprites/MovableSprite.java
@@ -4,7 +4,11 @@ public interface MovableSprite {
 	public void setCenterX(double centerX);
 	
 	public void setCenterY(double centerY);
+
+	public double getVelocityX();
 	
+	public double getVelocityY();
+
 	public void setVelocityX(double pixelsPerSecond);
 	
 	public void setVelocityY(double pixelsPerSecond);
